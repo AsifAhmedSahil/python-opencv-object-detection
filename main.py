db57cbdf4106f80d7bf56fdf014e90186e75fb59
EngineerSahil <77582744+EngineerSahil@users.noreply.github.com>
2022-09-21 17:26:26 +0600

    detection ready

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from pydoc import classname
 import cv2
 
 # openCv DNN
@@ -5,8 +6,29 @@ net =cv2.dnn.readNet("dnn_model/yolov4-tiny.weights","dnn_model/yolov4-tiny.cfg"
 model = cv2.dnn_DetectionModel(net)
 model.setInputParams(size = (320,320), scale = 1/255)
 
+# Load Class lists
+classes = []
+with open("dnn_model/classes.txt", "r") as file_object:
+    for class_name in file_object.readlines():
+        class_name = class_name.strip()
+        classes.append(class_name)
+print("Object Lists:")
+print(classes)
+
+
+
 # initialize camera
 cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH,1280)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT,720)
+
+def click_button(event, x,y,flags,params):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        print (x,y)
+
+# create window
+cv2.namedWindow("Frame")
+cv2.setMouseCallback("Frame",click_button)
 
 while True:
     # get frames***
@@ -16,8 +38,14 @@ while True:
     (class_ids,scores,bboxes)=model.detect(frame)
     for class_id, score, bbox in zip(class_ids,scores,bboxes):
         (x,y,w,h) = bbox
-        print(x,y,w,h)
+        class_name = classes[class_id]
+        
+        
+        cv2.putText(frame, class_name,(x,y-10),cv2.FONT_HERSHEY_PLAIN,1,(200,0,50),2)
+        
         cv2.rectangle(frame,(x,y), (x+w , y+h), (200,0,50), 3)
+        
+        
     
     
     print("class ids" ,class_ids)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+print("class ids" ,class_ids)
+    # print('scores',scores)
+    # print("bboxes",bboxes)
